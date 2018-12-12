4b8404832759f1e559367fc49f5f1e39d4fca000
Gilles Lima <gillestoim@gmail.com>
2018-12-12 16:42:09 -0200

    created authGuard

---
+++ src/http/authGuard.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+const authGuard = (deps) => {
+    return (req, res, next) => {
+        if (!deps.exclusions.includes(req.href())) {
+            const token = req.headers['x-access-token'];
+            if (!token) {
+                res.send(403, { error: 'Token não fornecido.' });
+                return false;
+            }
+            try {
+                req.decoded = jwt.verify(token, process.env.JWT_secret);
+            } catch (error) {
+                res.send(403, { error: 'Token inválido.' });
+                return false;
+            }
+        }
+
+        next();
+    }
+}
+
+module.exports = authGuard;
