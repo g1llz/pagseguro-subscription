56dd1cd293c50babafc89be15cf83a6998917da3
Gilles Lima <gillestoim@gmail.com>
2018-12-13 11:41:07 -0200

    updated error response; extract errorhandler

--- src/services/auth/auth.js
+++ src/services/auth/auth.js
@@ -8,11 +8,16 @@ const auth = deps => {
         authenticate: (email, password) => {
             return new Promise((resolve, reject) => {
                 User.find({ email: email, password: sha1(password) }).then((response) => {
-                    const { email, id } = response[0];
-                    const token = jwt.sign({ email, id }, process.env.JWT_secret, { expiresIn: 60 * 60 * 24 });
-                    resolve({ token });
+                    if (response.length) {
+                        const { email, id } = response[0];
+                        const token = jwt.sign({ email, id }, process.env.JWT_secret, { expiresIn: 60 * 60 * 24 });
+                        resolve({ token });
+                    } else {
+                        errorHandler(new Error('user not found or invalid password'), 'user not found.', reject);
+                        return false;
+                    }
                 }).catch((error) => {
-                    errorHandler(error, 'user not found.', reject);
+                    errorHandler(error, 'failed unexpected try again later.', reject);
                     return false;
                 })
             })
