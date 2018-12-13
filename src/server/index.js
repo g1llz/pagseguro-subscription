7298e25b92ccd7e967f25603964c8f14be08735a
Gilles Lima <gillestoim@gmail.com>
2018-12-13 11:43:38 -0200

    updated route

--- src/server/index.js
+++ src/server/index.js
@@ -5,7 +5,7 @@ const authGuard = require('../http/authGuard');
 
 const server = restify.createServer();
 
-const exclusions = ['/auth'];
+const exclusions = ['/api/v1/auth'];
 
 server.pre(cors.preflight);
 server.use(cors.actual);
