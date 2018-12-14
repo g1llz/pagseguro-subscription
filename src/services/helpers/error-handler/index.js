9bd4921d8a0cf2f36be72bb00b72fd35ba515993
Gilles Lima <gillestoim@gmail.com>
2018-12-14 11:14:03 -0200

    updated error handler

--- src/services/helpers/error-handler/index.js
+++ src/services/helpers/error-handler/index.js
@@ -1,11 +1,12 @@
 const convert = require('xml-js');
 
 const errorHandler = (err, rejectFunction, msg = '') => {
-    console.log(err || err.erros);
     if (!msg) {
-        err = convert.xml2js(err.error, { compact: true, spaces: 4 })
+        err = convert.xml2js(err.error, { compact: true, spaces: 4 });
+        err = err.errors;
     }
-    rejectFunction({ error: err.errors || msg });
+    rejectFunction({ error: err.error || msg });
+    console.log(err.error || err);
 }
 
 module.exports = errorHandler;
