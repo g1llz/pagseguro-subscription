b3720ad7eb5434c1d432a191cac3884a1d00cab5
Gilles Lima <gillestoim@gmail.com>
2018-12-14 13:56:57 -0200

    show in slack channel the error message

--- src/services/helpers/error-handler/index.js
+++ src/services/helpers/error-handler/index.js
@@ -1,9 +1,33 @@
 const convert = require('xml-js');
+const slack = require('slack-notify')(process.env.MY_SLACK_WEBHOOK_URL);
 
 const errorHandler = (err, rejectFunction, msg = '') => {
     if (!msg) {
         err = convert.xml2js(err.error, { compact: true, spaces: 4 });
         err = err.errors;
+        slack.send({
+            text: '*_(PagSeguro)_ ERROR*',
+            attachments: [
+                {
+                    fields: [
+                        { title: 'CODE', value: err.error.code._text, short: true },
+                        { title: 'MESSAGE', value: err.error.message._text, short: true }
+                    ]
+                }
+            ]
+        });
+    } else {
+        slack.send({
+            text: '*_(API)_ ERROR*',
+            attachments: [
+                {
+                    fields: [
+                        { title: 'MESSAGE', value: msg, short: false },
+                        { title: 'STACKTRACE', value: err.toString(), short: false }
+                    ]
+                }
+            ]
+        });
     }
     rejectFunction({ error: err.error || msg });
     console.log(err.error || err);
