f33de4206d27357ae450c8cf5df37cd1a5ddb42c
Gilles Lima <gillestoim@gmail.com>
2018-12-15 00:28:04 -0200

    updated notifications

--- src/services/pagseguro/notifications.js
+++ src/services/pagseguro/notifications.js
@@ -1,13 +1,16 @@
 const request = require('request-promise');
 const convert = require('xml-js');
 
+const headerTransaction = { 'Content-Type': 'application/json;charset=ISO-8859-1' }
+const headerPreApproval = { 'Content-Type': 'application/json;charset=ISO-8859-1', 'Accept': 'application/vnd.pagseguro.com.br.v3+xml;charset=ISO-8859-1' }
+
 const notification = deps => {
     const { errorHandler } = deps;
     return {
         receive: (code, type) => {
             const path = type === 'preApproval' ? 'pre-approvals' : 'v3/transactions';
             const options = {
-                headers: { 'Content-Type': 'application/json;charset=ISO-8859-1', 'Accept': 'application/vnd.pagseguro.com.br.v3+xml;charset=ISO-8859-1' },
+                headers: type === 'transaction' ? headerTransaction : headerPreApproval,
                 uri: `${process.env.PAG_url}/${path}/notifications/${type === 'transaction' ? formatCode(code) : code}`,
                 qs: { email: process.env.PAG_email, token: process.env.PAG_token },
                 method: 'GET'
@@ -28,7 +31,7 @@ const notification = deps => {
 }
 
 const formatCode = (code) => {
-    return [code.substring(0, 6), '-', code.substring(6, 18), '-', code.substring(19, 30), '-', code.substring(30, 36)].join('')
+    return [code.substring(0, 6), '-', code.substring(6, 18), '-', code.substring(18, 30), '-', code.substring(30, 36)].join('')
 }
 
 module.exports = notification;
