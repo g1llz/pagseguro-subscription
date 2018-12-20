65a80a1a1af11fcb349d204d7ccbaf885b61e871
Gilles Lima <gillestoim@gmail.com>
2018-12-20 19:35:05 -0200

    bug fix in payment-start; rename

--- src/services/pagseguro/index.js
+++ src/services/pagseguro/index.js
@@ -1,11 +1,11 @@
 const errorHandler = require('../helpers/error-handler');
 
-const start = require('./payment-start')({ errorHandler });
+const sessionModule = require('./payment-start')({ errorHandler });
 const subscriptionModule = require('./subscription')({ errorHandler });
 const notificationModule = require('./notifications')({ errorHandler });
 
 module.exports = {
+    session: () => sessionModule,
     subscription: () => subscriptionModule,
-    notification: () => notificationModule,
-    start
+    notification: () => notificationModule
 }
--- src/services/pagseguro/payment-start.js
+++ src/services/pagseguro/payment-start.js
@@ -1,23 +1,27 @@
 const request = require('request-promise');
 const convert = require('xml-js');
 
-const start = deps => {
+const session = deps => {
     const { errorHandler } = deps;
-    const options = {
-        headers: { 'Content-Type': 'application/json;charset=ISO-8859-1' },
-        uri: `${process.env.PAG_URL}/v2/sessions`,
-        qs: { email: process.env.PAG_EMAIL, token: process.env.PAG_TOKEN },
-        method: 'POST'
+    return {
+        start: () => {
+            const options = {
+                headers: { 'Content-Type': 'application/json;charset=ISO-8859-1' },
+                uri: `${process.env.PAG_URL}/v2/sessions`,
+                qs: { email: process.env.PAG_EMAIL, token: process.env.PAG_TOKEN },
+                method: 'POST'
+            }
+            return new Promise((resolve, reject) => {
+                request(options).then((res) => {
+                    let json = convert.xml2js(res, { compact: true, spaces: 4 })
+                    resolve(json.session.id._text);
+                }).catch((err) => {
+                    errorHandler(err, '', reject);
+                    return false;
+                });
+            })
+        }
     }
-    return new Promise((resolve, reject) => {
-        request(options).then((res) => {
-            let json = convert.xml2js(res, { compact: true, spaces: 4 })
-            resolve(json.session.id._text);
-        }).catch((err) => {
-            errorHandler(err, '', reject);
-            return false;
-        });
-    })
 }
 
-module.exports = start;
+module.exports = session;
