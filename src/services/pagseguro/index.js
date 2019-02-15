0315e089c5fd8792b895b8fde2e3b27d35440364
Gilles Lima <gillestoim@gmail.com>
2019-02-15 13:12:20 -0200

    create plan file; updated all methods that needs to xml2json

--- src/services/pagseguro/index.js
+++ src/services/pagseguro/index.js
@@ -1,10 +1,12 @@
 const errorHandler = require('../helpers/error-handler');
 
+const planModule = require('./plan')({ errorHandler });
 const sessionModule = require('./payment-start')({ errorHandler });
 const subscriptionModule = require('./subscription')({ errorHandler });
 const notificationModule = require('./notifications')({ errorHandler });
 
 module.exports = {
+    plan: () => planModule,
     session: () => sessionModule,
     subscription: () => subscriptionModule,
     notification: () => notificationModule
--- src/services/pagseguro/notifications.js
+++ src/services/pagseguro/notifications.js
@@ -1,5 +1,6 @@
 const request = require('request-promise');
 const convert = require('xml-js');
+const xml2Opt = require('../helpers/remove-text-attribute');
 const winston = require('winston');
 
 const logger = winston.createLogger({
@@ -30,7 +31,7 @@ const notification = deps => {
             return new Promise((resolve, reject) => {
                 request(options)
                     .then((res) => {
-                        res = convert.xml2js(res, { compact: true, spaces: 4 })
+                        res = convert.xml2js(res, xml2Opt)
                         const respRef = type === 'transaction' ? res.transaction : res.preApproval;
                         request({
                             uri: process.env.URL_NOTIFICATION,
