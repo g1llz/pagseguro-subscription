d05103b4c6ec55a9db70a49fbce25952dfc9b49d
Gilles Lima <gillestoim@gmail.com>
2019-04-29 16:23:30 -0300

    update routes

--- src/http/routes.js
+++ src/http/routes.js
@@ -3,25 +3,27 @@ const pg = require('../services/pagseguro');
 
 const routes = (server) => {
     server.get('/api/v1', (req, res, next) => {
-        res.send('silence is gold.');
+        res.json({ text: 'silence is gold.' });
         next();
     });
 
     server.post('/api/v1/auth', async (req, res, next) => {
         const { email, password } = req.body;
         try {
-            res.send(await db.auth().authenticate(email, password));
+            res.json(await db.auth().authenticate(email, password));
         } catch (error) {
-            res.send(401, error);
+            res.status(error.status);
+            res.json(error);
         }
         next();
     });
 
-    server.post('/api/v1/start-payment', async (req, res, next) => {
+    server.get('/api/v1/start-payment', async (req, res, next) => {
         try {
-            res.send(await pg.session().start());
+            res.json(await pg.session().start());
         } catch (error) {
-            res.send(401, error);
+            res.status(error.status);
+            res.json(error);
         }
         next();
     });
@@ -29,9 +31,10 @@ const routes = (server) => {
     server.post('/api/v1/plan/create', async (req, res, next) => {
         const { plan } = req.body;
         try {
-            res.send(await pg.plan().create(plan));
+            res.json(await pg.plan().create(plan));
         } catch (error) {
-            res.send(400, error);
+            res.status(error.status);
+            res.json(error);
         }
         next();
     });
@@ -39,29 +42,32 @@ const routes = (server) => {
     server.post('/api/v1/subscription/new', async (req, res, next) => {
         const { customer } = req.body;
         try {
-            res.send(await pg.subscription().new(customer));
+            res.json(await pg.subscription().new(customer));
         } catch (error) {
-            res.send(400, error);
+            res.status(error.status);
+            res.json(error);
         }
         next();
     });
 
-    server.post('/api/v1/subscription/orders', async (req, res, next) => {
-        const { code } = req.body;
+    server.get('/api/v1/subscription/:code/orders', async (req, res, next) => {
+        const { code } = req.params;
         try {
-            res.send(await pg.subscription().ordersByApprovalCode(code));
+            res.json(await pg.subscription().ordersByApprovalCode(code));
         } catch (error) {
-            res.send(400, error);
+            res.status(error.status);
+            res.json(error);
         }
         next();
     });
 
-    server.post('/api/v1/subscription/detail', async (req, res, next) => {
-        const { code } = req.body;
+    server.get('/api/v1/subscription/:code/detail', async (req, res, next) => {
+        const { code } = req.params;
         try {
-            res.send(await pg.subscription().signatureDetailByApprovalCode(code));
+            res.json(await pg.subscription().signatureDetailByApprovalCode(code));
         } catch (error) {
-            res.send(400, error);
+            res.status(error.status);
+            res.json(error);
         }
         next();
     });
@@ -69,19 +75,21 @@ const routes = (server) => {
     server.post('/api/v1/subscription/order-discount', async (req, res, next) => {
         const { code, discount } = req.body;
         try {
-            res.send(await pg.subscription().discountInNextOrder(code, discount));
+            res.json(await pg.subscription().discountInNextOrder(code, discount));
         } catch (error) {
-            res.send(400, error);
+            res.status(error.status);
+            res.json(error);
         }
         next();
     });
 
-    server.post('/api/v1/subscription/cancel', async (req, res, next) => {
-        const { code } = req.body;
+    server.put('/api/v1/subscription/:code/cancel', async (req, res, next) => {
+        const { code } = req.params;
         try {
-            res.send(await pg.subscription().cancel(code));
+            res.json(await pg.subscription().cancel(code));
         } catch (error) {
-            res.send(400, error);
+            res.status(error.status);
+            res.json(error);
         }
         next();
     });
@@ -89,29 +97,31 @@ const routes = (server) => {
     server.post('/api/v1/notifications', async (req, res, next) => {
         const { notificationCode, notificationType } = req.body;
         try {
-            res.send(await pg.notification().receive(notificationCode, notificationType))
+            res.json(await pg.notification().receive(notificationCode, notificationType))
         } catch (error) {
-            res.send(400, error);
+            res.status(error.status);
+            res.json(error);
         }
         next();
     });
 
     server.get('/api/v1/users', async (req, res, next) => {
         try {
-            res.send(await db.user().all());
+            res.json(await db.user().all());
         } catch (error) {
-            res.send(404, error);
+            res.status(error.status);
+            res.json(error);
         }
         next();
     });
 
     server.post('/api/v1/users', async (req, res, next) => {
         const { email, password } = req.body;
-        console.log(email, password)
         try {
-            res.send(await db.user().save(email, password));
+            res.json(await db.user().save(email, password));
         } catch (error) {
-            res.send(400, error);
+            res.status(error.status);
+            res.json(error);
         }
         next();
     });
